c93e63d5dec987a5429be8d4515a3501ec7b4938
vldx4k <vldx4k@org>
2024-03-04 23:38:49 +0200

    Lab2 function rewrite

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -2,27 +2,24 @@
 #include<math.h>
 using namespace std;
 
-void radius(float* x, float* y);
-void angle(float* x, float* y);
+float radius(float* x, float* y);
+float angle(float* x, float* y);
 
 int main() {
 
 float x, y;
     cout << "enter the x coordinate: "; cin >> x;
     cout << "enter the y coordinate: "; cin >> y;
-    radius(&x, &y);
-    angle(&x, &y);
+    cout << "  radius = " << radius(&x, &y) << '\n';
+    cout << "  angle = "  << angle(&x, &y)  << '\n';
 
     return 0;
 }
 
-void radius(float* x, float* y) {
-    float radius = sqrt((*x)*(*x) + (*y)*(*y));
-    cout << "  radius = " << radius << '\n';
+float radius(float* x, float* y) {
+    return sqrt((*x)*(*x) + (*y)*(*y));
 }
 
-void angle(float* x, float* y) {
-    float theta = atan((*y) / (*x));
-    float angle = theta * (180/M_PI);
-    cout << "   angle = " << angle << '\n';
+float angle(float* x, float* y) {
+    return atan((*y) / (*x))  * (180/M_PI);
 }
